da64e76c0745b2b0068071e7eee07cc6856b2502
César Castillo <cesar.castillo@balanceinteractive.com>
2016-10-06 17:52:30 -0500

    Committed by DevDesktop

--- docroot/modules/custom/balance_helper/js/search-form.js
+++ docroot/modules/custom/balance_helper/js/search-form.js
@@ -3,9 +3,9 @@
   Drupal.behaviors.ghsaSearchForm = {
     attach: function (context, settings) {
 
-      // Add placeholder to search form
-      $('#search-block-form input').attr('placeholder', 'search');
-
+      // Add placeholder to search form and change button text.
+      $('#search-block-form .form-search').attr('placeholder', 'search');
+      $('#search-block-form .button').attr('value', 'Search Query');
     }
   };
 }(jQuery));
