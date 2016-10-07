3bba90a2622f3e29e65dcdd68649cff8b55bd670
César Castillo <cesar.castillo@balanceinteractive.com>
2016-10-07 16:57:01 -0500

    Committed by DevDesktop

--- docroot/modules/custom/balance_helper/js/search-form.js
+++ docroot/modules/custom/balance_helper/js/search-form.js
@@ -5,7 +5,7 @@
 
       // Add placeholder to search form and change button text.
       $('#search-block-form .form-search').attr('placeholder', 'search');
-      $('#search-block-form .button').attr('value', 'Search Query');
+      // $('#search-block-form .button').attr('value', 'Search Query');
     }
   };
 }(jQuery));
